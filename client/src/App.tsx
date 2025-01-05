5664a2ff444f08b0e50d6c3a80fde2102989522f
kaeytee <austinbediako4@gmail.com>
2025-01-05 14:07:02 +0000

    Initialize React app with Tailwind CSS, add basic structure and styles

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className='main'>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  );
+};
+
+export default App;
