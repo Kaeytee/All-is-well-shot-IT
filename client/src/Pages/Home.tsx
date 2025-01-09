e4b4f8f85f4c832eb5edd4d0b246fa215fc23338
kaeytee <austinbediako4@gmail.com>
2025-01-09 22:09:07 +0000

    Add HomeHero component with image gallery and text; update Home page to include HomeHero

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -1,30 +1,13 @@
 import React from 'react';
 import './Styles/Home.css';
+import HomeHero from '../Components/HomeHero/HomeHero.tsx';
 import CustomerTestimonials from '../Components/CustomerTestimonials/CustomerTestimonials.tsx';
-import homeImage1 from '../Assets/homeImage1.png';
-import homeImage2 from '../Assets/homeImage2.png';
-import homeImage3 from '../Assets/homeImage3.png';
-import homeImage4 from '../Assets/homeImage4.png';
-import homeImage5 from '../Assets/homeImage5.png';
-import homeImage6 from '../Assets/homeImage6.png';
+
 
 const Home = () => {
   return (
     <div className=" justify-center items-center ">
-      <div className="gallery flex flex-col flex-1   overflow-hidden  bg-gray-500">
-        <div className="topdiv flex justify-evenly">
-
-        <img src={homeImage1} alt="homeImage1" className="image1 -translate-x-44  translate-y-48 " />
-        <img src={homeImage2} alt="homeImage2" className="image2 -translate-x-56 translate-y-5" />
-        <img src={homeImage3} alt="homeImage3" className="image3 -translate-x-72 -translate-y-36" />
-        </div>
-        <div className="downdiv flex justify-evenly">
-
-        <img src={homeImage4} alt="homeImage4" className="image4 translate-x-32 translate-y-10 " />
-        <img src={homeImage5} alt="homeImage5" className="image5  translate-x-20 -translate-y-32" />
-        <img src={homeImage6} alt="homeImage6" className="image6  translate-x-9 -translate-y-80" />
-        </div>
-      </div>
+      <HomeHero />
       <CustomerTestimonials />
     </div>
   );
