6b0ccb1b1ddca325f699602fc98a130b8a776abb
kaeytee <austinbediako4@gmail.com>
2025-01-09 18:45:35 +0000

    completed the homescreen transitions

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -14,15 +14,15 @@ const Home = () => {
       <div className="gallery flex flex-col flex-1   overflow-hidden  bg-gray-500">
         <div className="topdiv flex justify-evenly">
 
-        <img src={homeImage1} alt="homeImage1" className="image1 -translate-x-44 translate-y-36 " />
-        <img src={homeImage2} alt="homeImage2" className="image2 -translate-x-72 -translate-y-6" />
-        <img src={homeImage3} alt="homeImage3" className="image3 -translate-x-96 -translate-y-48" />
+        <img src={homeImage1} alt="homeImage1" className="image1 -translate-x-44  translate-y-48 " />
+        <img src={homeImage2} alt="homeImage2" className="image2 -translate-x-56 translate-y-5" />
+        <img src={homeImage3} alt="homeImage3" className="image3 -translate-x-72 -translate-y-36" />
         </div>
         <div className="downdiv flex justify-evenly">
 
-        <img src={homeImage4} alt="homeImage4" className="image4 translate-x-36 " />
-        <img src={homeImage5} alt="homeImage5" className="image5  translate-x-24 -translate-y-48" />
-        <img src={homeImage6} alt="homeImage6" className="image6  translate-x-9 -translate-y-96" />
+        <img src={homeImage4} alt="homeImage4" className="image4 translate-x-32 translate-y-10 " />
+        <img src={homeImage5} alt="homeImage5" className="image5  translate-x-20 -translate-y-32" />
+        <img src={homeImage6} alt="homeImage6" className="image6  translate-x-9 -translate-y-80" />
         </div>
       </div>
       <CustomerTestimonials />
