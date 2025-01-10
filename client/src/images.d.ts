ca84b61665d04217c9266eb3ad62eedcd60c76f2
kaeytee <austinbediako4@gmail.com>
2025-01-10 16:55:31 +0000

    Add support for additional media file types in images.d.ts

--- client/src/images.d.ts
+++ client/src/images.d.ts
@@ -1,29 +1,49 @@
 declare module "*.png" {
-	const content: string;
-	export default content;
-  }
-  
-  declare module "*.jpg" {
-	const content: string;
-	export default content;
-  }
-  
-  declare module "*.jpeg" {
-	const content: string;
-	export default content;
-  }
-  
-  declare module "*.svg" {
-	const content: string;
-	export default content;
-  }
-  
-  declare module "*.gif" {
-	const content: string;
-	export default content;
-  }
-  
-  declare module "*.webp" {
-	const content: string;
-	export default content;
-  }
+    const content: string;
+    export default content;
+}
+
+declare module "*.jpg" {
+    const content: string;
+    export default content;
+}
+
+declare module "*.jpeg" {
+    const content: string;
+    export default content;
+}
+
+declare module "*.svg" {
+    const content: string;
+    export default content;
+}
+
+declare module "*.gif" {
+    const content: string;
+    export default content;
+}
+
+declare module "*.webp" {
+    const content: string;
+    export default content;
+}
+
+declare module "*.mp4" {
+    const content: string;
+    export default content;
+}
+
+declare module "*.avi" {
+    const content: string;
+    export default content;
+}
+
+declare module "*.mov" {
+    const content: string;
+    export default content;
+}
+
+declare module "*.wmv" {
+    const content: string;
+    export default content;
+}
